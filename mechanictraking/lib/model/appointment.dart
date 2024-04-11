1380f81e78096ef02e30028bccf3e3e842195f28
Americo <agzz2003@gmail.com>
2024-04-11 05:05:07 -0600

    Creacion de servicio para poder obtener de firebase los datos y poder pasarlos con mi metodos .fromJson a mi modelo local appointment.dart. Creacion de metodo getAppointment para recibir una cita en especifico

--- mechanictraking/lib/model/appointment.dart
+++ mechanictraking/lib/model/appointment.dart
@@ -1,8 +1,22 @@
 class Appointment {
+  final String id;
   final String auto;
-  final String date;
+  //final String date;
   final String motivo;
   final bool status;
 
-  Appointment(this.auto, this.date, this.motivo, this.status);
+  Appointment(this.id, this.auto, this.motivo, this.status);
+
+  Appointment.fromJson(String id, Map<String, dynamic> json)
+      : //dynamic es el contenido y el string son las key
+        this(
+          id,
+          json['automovil'] as String, //se debe parsear cada elemento
+          //json['date'] as String,
+          json['motivo'] as String,
+          json['status'] as bool,
+        );
+  toJson() {
+    throw Exception();
+  }
 }
---
+++ mechanictraking/lib/services/appointment_service.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mechanictracking/model/appointment.dart';
+
+class AppointmentService {
+  //obtener un documento particular de firebase(tabla)
+  //se crea la referencia (el path) del nombre de la collection(tabla)
+  //.get se obtienen todos las citas
+  //withConverter ayuda a convertir de inmediato la data (snapshot (objeto que devuelve firestore) a nuestras clases
+  final appointmentRef = FirebaseFirestore.instance
+      .collection('citas')
+      .withConverter(
+        fromFirestore: (snapshot, _) =>
+            Appointment.fromJson(snapshot.id, snapshot.data()!),
+        toFirestore: (citas, _) => citas
+            .toJson(), //recibe como parametro un modelo y lo convierto a Json con mi metodo toJson de appointment
+      ); //convierto de firestore a mis clases con mi metodo hecho en appointment
+
+  //TODO modificar este metodo para obtener todas las citas con valor pendiente en status
+  Future<List<Appointment>> getAllAppointment() async {
+    //nos devuelve un future que devuelve una lista de citas
+    //apartir de mi referencia obtengo los datos y regresa la respuesta (query que contiene book)
+    var result = await appointmentRef.get().then((value) => value);
+    //obtiene los tres ultimos
+    List<Appointment> appointment = []; //lista vacia
+
+    for (var doc in result.docs) {
+      //se itera para cada uno de los docs(tablas) que vienen en mi resultado
+      //se agrega la cita que viene en cada doc en su data a mi lista de citas appointment
+      appointment.add(doc.data());
+    }
+    //refresa un future value de la lista de libros obtenida
+    return Future.value(appointment);
+  }
+
+  Future<Appointment> getAppointment(String appointmentId) async {
+    //nos devuelve un future que devuelve una lista de libros
+    // obtiene un libro especifico a partir de su id
+    var result =
+        await appointmentRef.doc(appointmentId).get().then((value) => value);
+
+    if (result.exists) {
+      return Future.value(result.data());
+    }
+    throw const HttpException("Cita no encontrado");
+  }
+}
