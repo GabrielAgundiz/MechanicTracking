258a3012e932b36db7da8524cb9ecc8f41e93cee
GabrielAgundiz <gabrielg.agundizh@gmail.com>
2024-05-12 17:01:53 -0600

    Tracking Act Form

--- mechanictraking/lib/screens/admin/trackformad.dart
+++ mechanictraking/lib/screens/admin/trackformad.dart
@@ -12,6 +12,7 @@ class TrackFormAD extends StatefulWidget {
 class _TrackFormADState extends State<TrackFormAD> {
   //estado de la cita
   final _formKey = GlobalKey<FormState>();
+  bool _includeDiagnostic = false;
   String _model = '';
   String _reason = '';
   DateTime _selectedDate = DateTime.now();
@@ -143,18 +144,180 @@ class _TrackFormADState extends State<TrackFormAD> {
                     alignment: Alignment.centerLeft,
                     child: Row(
                       children: [
-                        Text(
+                        const Text(
                           "¿Incluye Diagnóstico?",
                           style: TextStyle(
                               fontSize: 16, fontWeight: FontWeight.bold),
                         ),
-                        
+                        Checkbox(
+                          value:
+                              _includeDiagnostic, // Add a boolean variable to store the checkbox state
+                          onChanged: (value) {
+                            setState(() {
+                              _includeDiagnostic = value!;
+                            });
+                          },
+                        ),
                       ],
                     ),
                   ),
                   const SizedBox(
                     height: 30,
                   ),
+                  _includeDiagnostic
+                      ? Container(
+                          alignment: Alignment.centerLeft,
+                          child: Column(
+                            children: [
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                child: const Text(
+                                  "Automovil:",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 6,
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                    hintText: 'Modelo del Automovil',
+                                    hintStyle: TextStyle(fontSize: 14)),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Por favor, ingrese un motivo';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) => _reason = value!,
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                child: const Text(
+                                  "Reparacion:",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 6,
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                    hintText: 'Nombre de la reparacion',
+                                    hintStyle: TextStyle(fontSize: 14)),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Por favor, ingrese un motivo';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) => _reason = value!,
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                child: const Text(
+                                  "Descripcion:",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 6,
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                    hintText: 'Descripcion de la reparacion',
+                                    hintStyle: TextStyle(fontSize: 14)),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Por favor, ingrese un motivo';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) => _reason = value!,
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                child: const Text(
+                                  "Costo:",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 6,
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                    hintText: 'Costo de la reparacion MXN',
+                                    hintStyle: TextStyle(fontSize: 14)),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Por favor, ingrese un motivo';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) => _reason = value!,
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                child: const Text(
+                                  "Imagenes adjuntas:",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 6,
+                              ),
+                              InkWell(
+                                onTap: () {},
+                                child: Container(
+                                  width: 115,
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 12),
+                                  decoration: BoxDecoration(
+                                    color: Colors.green[400],
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: const Center(
+                                    child: Text(
+                                      "Agregar",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 50,
+                              ),
+                            ],
+                          ),
+                        )
+                      : Container(),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
