8074a20240b45230ac5ffd492cbe6c585214f94d
Americo <agzz2003@gmail.com>
2024-05-12 20:18:14 -0600

    Inicio de sesion para admins

--- mechanictraking/lib/screens/login.dart
+++ mechanictraking/lib/screens/login.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:email_validator/email_validator.dart'; // Importa el paquete email_validator para validar correos electrónicos.
 import 'package:firebase_auth/firebase_auth.dart'; // Importa la autenticación de Firebase.
 import 'package:flutter/material.dart'; // Importa el paquete flutter material.
+import 'package:mechanictracking/screens/admin/homead.dart';
 import 'package:mechanictracking/screens/recovery.dart'; // Importa la pantalla de recuperación de contraseña.
 import 'package:mechanictracking/screens/register.dart'; // Importa la pantalla de registro.
 import 'package:mechanictracking/screens/user/home.dart'; // Importa la pantalla de inicio de la aplicación.
@@ -51,14 +53,36 @@ class _LoginPageState extends State<LoginPage> {
           child: CircularProgressIndicator(),
         ),
       );
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: _emailController.text.trim(),
         password: _passwordController.text.trim(),
       );
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => HomePage()),
-      );
+
+      final User? user = userCredential.user;
+      final credencial = user?.uid;
+      if (user != null) {
+        final userDoc = await FirebaseFirestore.instance
+            .collection('admin')
+            .doc(credencial)
+            .get();
+
+        if (userDoc.exists) {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => HomePageAD()),
+            (route) =>
+                false, // Elimina todas las rutas de navegación anteriores
+          );
+        } else {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+            (route) =>
+                false, // Elimina todas las rutas de navegación anteriores
+          );
+        }
+      }
     } on FirebaseAuthException catch (e) {
       print(e);
       Navigator.pop(context);
@@ -85,7 +109,7 @@ class _LoginPageState extends State<LoginPage> {
         );
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(e.message ?? 'Error desconocido')),
+          SnackBar(content: Text('Error al iniciar sesión, intente de nuevo')),
         );
       }
     }
@@ -95,144 +119,146 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(20.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Accede',
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-                const SizedBox(height: 40.0),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextFormField(
-                    controller: _emailController,
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                      hintText: 'example@email.com',
-                      prefixIcon: Icon(Icons.email),
-                      border: InputBorder.none,
-                      contentPadding:
-                          EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                    ),
-                    validator: (email) =>
-                        email != null && !EmailValidator.validate(email)
-                            ? 'Ingresa un correo válido'
-                            : null,
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextFormField(
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      labelText: 'Contraseña',
-                      hintText: '••••••••',
-                      prefixIcon: const Icon(Icons.lock),
-                      suffixIcon: IconButton(
-                        icon: Icon(_obscureText
-                            ? Icons.visibility_off
-                            : Icons.remove_red_eye),
-                        onPressed: () {
-                          setState(() {
-                            _obscureText = !_obscureText;
-                          });
-                        },
-                      ),
-                      border: InputBorder.none,
-                      contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 12),
-                    ),
-                    obscureText: _obscureText,
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    validator: (value) => value != null && value.length < 8
-                        ? 'Contraseña Incorrecta'
-                        : null,
-                  ),
-                ),
-                const SizedBox(height: 40.0),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(double.infinity, 50),
-                    backgroundColor: Colors.green[400],
-                    foregroundColor: Colors.white,
-                  ),
-                  child: const Text('Iniciar sesión'),
-                  onPressed: () async {
-                    try {
-                      await signIn();
-                    } catch (e) {
-                      if (e is FirebaseAuthException &&
-                          e.code == 'wrong-password') {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('Contraseña incorrecta'),
-                            backgroundColor: Colors.red,
-                          ),
-                        );
-                      }
-                    }
-                  },
-                ),
-                const SizedBox(height: 40.0),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => RecoverPasswordPage()),
-                    );
-                  },
-                  child: Text(
-                    '¿Olvidaste tu contraseña?',
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Accede',
                     style: TextStyle(
-                      color: const Color(0xFF5DB075),
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
                   ),
-                ),
-                const SizedBox(height: 20.0),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => RegisterPage()),
-                    );
-                  },
-                  child: RichText(
-                    text: TextSpan(
-                      text: '¿Aún no tienes una cuenta? ',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black,
+                  const SizedBox(height: 40.0),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextFormField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'example@email.com',
+                        prefixIcon: Icon(Icons.email),
+                        border: InputBorder.none,
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 16, vertical: 12),
                       ),
-                      children: [
-                        TextSpan(
-                          text: 'Regístrate',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: const Color(0xFF5DB075),
-                          ),
-                        ),
-                      ],
+                      validator: (email) =>
+                          email != null && !EmailValidator.validate(email)
+                              ? 'Ingresa un correo válido'
+                              : null,
                     ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 20.0),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextFormField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        labelText: 'Contraseña',
+                        hintText: '••••••••',
+                        prefixIcon: const Icon(Icons.lock),
+                        suffixIcon: IconButton(
+                          icon: Icon(_obscureText
+                              ? Icons.visibility_off
+                              : Icons.remove_red_eye),
+                          onPressed: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                        ),
+                        border: InputBorder.none,
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 12),
+                      ),
+                      obscureText: _obscureText,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) => value != null && value.length < 8
+                          ? 'Contraseña Incorrecta'
+                          : null,
+                    ),
+                  ),
+                  const SizedBox(height: 40.0),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 50),
+                      backgroundColor: Colors.green[400],
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Iniciar sesión'),
+                    onPressed: () async {
+                      try {
+                        await signIn();
+                      } catch (e) {
+                        if (e is FirebaseAuthException &&
+                            e.code == 'wrong-password') {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Contraseña incorrecta'),
+                              backgroundColor: Colors.red,
+                            ),
+                          );
+                        }
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 40.0),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => RecoverPasswordPage()),
+                      );
+                    },
+                    child: Text(
+                      '¿Olvidaste tu contraseña?',
+                      style: TextStyle(
+                        color: const Color(0xFF5DB075),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => RegisterPage()),
+                      );
+                    },
+                    child: RichText(
+                      text: TextSpan(
+                        text: '¿Aún no tienes una cuenta? ',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'Regístrate',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: const Color(0xFF5DB075),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
