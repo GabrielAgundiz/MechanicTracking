2169daf2f0ca5a8d69268a28fbb14eb1f6145d11
GabrielAgundiz <gabrielg.agundizh@gmail.com>
2024-05-14 14:16:22 -0600

    Padding Bug Fixes

--- mechanictraking/lib/screens/user/widgets/actualtracking.dart
+++ mechanictraking/lib/screens/user/widgets/actualtracking.dart
@@ -106,124 +106,127 @@ class _CardAppointmentState extends State<CardAppointment> {
     if (_appointment == null) {
       return const Center(child: CircularProgressIndicator());
     } else if (_appointment!.status == "Pendiente") {
-      return Container(
-        padding: const EdgeInsets.symmetric(vertical: 5),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 4,
-              spreadRadius: 2,
-            ),
-          ],
-        ),
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: [
-              ListTile(
-                title: Text(
-                  _appointment!.auto,
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                subtitle: Text(_appointment!.motivo),
-                trailing: CircleAvatar(
-                  radius: 25,
-                  backgroundImage: NetworkImage(
-                      "https://patiodeautos.com/wp-content/uploads/2018/09/6-consejos-para-convertirte-en-un-mejor-mecanico-de-autos.jpg"),
-                ),
+      return Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 5),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 4,
+                spreadRadius: 2,
               ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 15),
-                child: Divider(
-                  thickness: 1,
-                  height: 20,
+            ],
+          ),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text(
+                    _appointment!.auto,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(_appointment!.motivo),
+                  trailing: CircleAvatar(
+                    radius: 25,
+                    backgroundImage: NetworkImage(
+                        "https://patiodeautos.com/wp-content/uploads/2018/09/6-consejos-para-convertirte-en-un-mejor-mecanico-de-autos.jpg"),
+                  ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "Completado: ",
-                        style: TextStyle(
-                          color: Colors.black54,
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Divider(
+                    thickness: 1,
+                    height: 20,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 10,
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.calendar_month,
-                        color: Colors.black54,
-                      ),
-                      SizedBox(width: 5),
-                      Text(
-                        DateFormat('dd/MM/yyyy').format(_appointment!.date),
-                        style: TextStyle(color: Colors.black54),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.access_time_filled,
-                        color: Colors.black54,
-                      ),
-                      SizedBox(width: 5),
-                      Text(
-                        DateFormat.jm().format(_appointment!.date),
-                        style: TextStyle(
-                          color: Colors.black54,
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      _openTrackingDetails(context, _appointment);
-                    },
-                    child: Container(
-                      width: 300,
-                      padding: EdgeInsets.symmetric(vertical: 12),
-                      decoration: BoxDecoration(
-                        color: Colors.green[300],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Ver detalles",
+                        Text(
+                          "Completado: ",
                           style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.calendar_month,
+                          color: Colors.black54,
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          DateFormat('dd/MM/yyyy').format(_appointment!.date),
+                          style: TextStyle(color: Colors.black54),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.access_time_filled,
+                          color: Colors.black54,
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          DateFormat.jm().format(_appointment!.date),
+                          style: TextStyle(
+                            color: Colors.black54,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        _openTrackingDetails(context, _appointment);
+                      },
+                      child: Container(
+                        width: 300,
+                        padding: EdgeInsets.symmetric(vertical: 12),
+                        decoration: BoxDecoration(
+                          color: Colors.green[300],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Ver detalles",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-            ],
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+              ],
+            ),
           ),
         ),
       );
